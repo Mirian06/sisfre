6ad366216f627371d44546a174e8ced311a072fd
Mirian06 <stephanny.mirian.pinheiro08@aluno.ifce.edu.br>
2022-03-27 16:27:31 -0300

    Removendo barra de navegação do menu

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.base')
+@extends('layout.baseNew')
 
 @section('titulo','Login')
 
@@ -7,10 +7,10 @@
 @endsection
 
 @section('conteudo')
-        <div class="wrapper container-fluid">
+        <div class="menu">
             <form action={{route('login')}} method="POST" class="form-signin">
 
-                <h2 class="form-login-heading"> <img src="/img/newLogo.png"> </h2>
+                <h2 class="form-login-heading"> <img src="img/newLogo.png"> </h2>
                 <hr class="colorgraph"><br>
                 <div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
                     <label for="email" class="control-label">E-mail: </label>
